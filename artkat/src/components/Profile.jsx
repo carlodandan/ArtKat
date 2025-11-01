790d5ea12a1ae71c17854c30f4efa538cdf38ece
Carlo Dee <carlodee.official@proton.me>
2025-11-01 14:56:00 +0800

    Testing new color scheme.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- artkat/src/components/Profile.jsx
+++ artkat/src/components/Profile.jsx
@@ -33,9 +33,13 @@ const Profile = () => {
             <h3>Skills & Software</h3>
             <div className="skills__grid">
               <div className="skill-item">
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <rect x="2" y="2" width="20" height="20" stroke="currentColor" strokeWidth="2"/>
-                </svg>
+                <img 
+                  src="/csp.png" 
+                  alt="Clip Studio Paint" 
+                  className="skill-icon skill-icon-circle"
+                  width="24" 
+                  height="24"
+                />
                 <span>Clip Studio Paint</span>
               </div>
             </div>
--- artkat/src/main.jsx
+++ artkat/src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './App.css'
+import './styles/base.css';
+import './styles/components.css';
+import './styles/sections.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
